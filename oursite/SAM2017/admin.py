f97e325bc1f8118024b6477fdaab7e24b36b25dc
heenasurve <hfsurve@hotmail.com>
2016-11-29 12:33:41 -0500

    Removed the Author model from the Admin site

--- oursite/SAM2017/admin.py
+++ oursite/SAM2017/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Paper, Author, PCC, PCM, Notification
+from .models import Paper, PCC, PCM, Notification
 admin.site.register(Paper)
-admin.site.register(Author)
+#admin.site.register(Author)
 admin.site.register(PCC)
 admin.site.register(PCM)
 admin.site.register(Notification)
